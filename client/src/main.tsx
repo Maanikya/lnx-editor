297a5fd6b848c5c8f7b5dcdc78562db874424e70
Maanikya <maanikya.acharya@gmail.com>
2025-07-06 17:23:33 +0530

    Vercel Analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/doc/:id" element={<App />} />
     </Routes>
+    <Analytics />
   </BrowserRouter>
 )
