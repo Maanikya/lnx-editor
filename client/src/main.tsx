f88c0cdec18fc9f70b3c7b672c7c54ed7a504746
Maanikya <maanikya.acharya@gmail.com>
2025-07-06 17:27:52 +0530

    Uninstall Vercel Analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
-  <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/doc/:id" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-    <Analytics />
-  </>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/doc/:id" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 )
